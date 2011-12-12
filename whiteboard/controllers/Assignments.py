c909545f63533a0e8d995664efd0f6cd93aa8569
Katie Kloss <kloss.katherine@gmail.com>
2011-12-11 23:20:14 -0500

    Allow files to be uploaded as part of an Assignment

--- whiteboard/controllers/Assignments.py
+++ whiteboard/controllers/Assignments.py
@@ -2,6 +2,7 @@ import whiteboard.template
 import whiteboard.helpers.RoleHelper as RoleHelper
 import whiteboard.helpers.CourseHelper as CourseHelper
 import whiteboard.helpers.AssignmentHelper as AssignmentHelper
+import whiteboard.helpers.DocumentHelper as DocumentHelper
 import whiteboard.sqltool
 
 import cherrypy
@@ -17,8 +18,18 @@ class Assignments:
             return whiteboard.template.render('error.html', context_dict =
                 {'error': 'The specified course does not exist'})
 
-        ctx['assignments'] = AssignmentHelper.fetch_assignments_for_course(courseid)
+        ctx['assignments'] = []
+        sql = whiteboard.sqltool.SqlTool()
+        sql.query_text = """SELECT * FROM Assignments A LEFT OUTER JOIN
+        (
+            SELECT * FROM Documents WHERE name LIKE 'assignment_%%'
+        ) AS D
+        ON A.assignmentid = D.assignmentid
+        WHERE A.courseid = @courseid"""
 
+        sql.addParameter("@courseid", courseid)
+        with sql.execute() as datareader:
+            ctx['assignments'] = [x for x in datareader]
         return whiteboard.template.render('assignments.html', context_dict=ctx)
 
     @RoleHelper.require_role('instructor', 'Only instructors are permitted to create assignments')
@@ -32,19 +43,44 @@ class Assignments:
         return whiteboard.template.render('createassignment.html', context_dict=ctx)
 
     @RoleHelper.require_role('instructor', 'Only instructors are permitted to create assignments')
-    def createAssignment_POST(self, courseid, title, duedate, points):
+    def createAssignment_POST(self, courseid, title, duedate, points, upload = None):
 
         if title == '' or duedate == '' or points == '':
             raise cherrypy.HTTPRedirect("createassignment")
 
         sql = whiteboard.sqltool.SqlTool()
-        sql.query_text = "INSERT INTO Assignments (title, due, points, courseid) VALUES (@title, @duedate, @points, @courseid)"
+        sql.query_text = "INSERT INTO Assignments (title, due, points, courseid) VALUES (@title, @duedate, @points, @courseid); SELECT LASTVAL() AS id;"
         sql.addParameter("@title", title)
         sql.addParameter("@duedate", duedate)
         sql.addParameter("@points", points)
         sql.addParameter("@courseid", courseid)
+        with sql.execute() as datareader:
+            assignment_id = datareader.fetch()['id']
+        
+        # TODO: This is painfully distasteful (and that's an understatement)
+
+        # Basically, the DocumentHelper instance later on (if one exists) uses a second DB
+        # connection which is isolated at READ COMMITTED, and we don't commit
+        # until the first SqlTool disconnects, so this assignment won't exist in time
+        # to create a Document referencing it unless we force a commit
+        sql.query_text = "COMMIT"
         sql.execute()
 
+        if upload != None:
+            path = "files/%s/assignments/" % courseid
+            ext = os.path.splitext(upload.filename)[1]
+            new_name = "assignment_%s%s" % (assignment_id, ext)
+            document_id = DocumentHelper.create_file(new_name, path, courseid, assignment_id)
+            
+            if not os.path.exists(path):
+                os.makedirs(path)
+            with open(path + new_name, "wb") as output:
+                while True:
+                    data = upload.file.read(8192)
+                    if not data:
+                        break
+                    output.write(data)
+            
         raise cherrypy.HTTPRedirect("assignments")
 
     @RoleHelper.require_role('student', 'Only students enrolled in this course may respond to its assignments')
--- whiteboard/helpers/DocumentHelper.py
+++ whiteboard/helpers/DocumentHelper.py
@@ -3,6 +3,23 @@ import whiteboard.sqltool
 class DocumentHelper:
 
     @staticmethod
+    def create_file(name, path, courseid, assignmentid):
+        
+        sql = whiteboard.sqltool.SqlTool()
+        if assignmentid != None:
+            sql.query_text = "INSERT INTO Documents (isfolder, name, path, courseid, assignmentid) VALUES (False, @name, @path, @courseid, @assignmentid)"
+            sql.addParameter("@assignmentid", assignmentid)
+        else:
+            sql.query_text = "INSERT INTO Documents (isfolder, name, path, courseid) VALUES (False, @name, @path, @courseid)"
+        sql.addParameter("@name", name)
+        sql.addParameter("@path", path)
+        sql.addParameter("@courseid", courseid)
+        sql.execute()
+        sql.query_text = "SELECT LASTVAL() as id"
+        with sql.execute() as datareader:
+            return datareader.fetch()['id']
+        
+    @staticmethod
     def get_file(documentid):
         sql = whiteboard.sqltool.SqlTool()
         sql.query_text = "SELECT * FROM Documents WHERE documentid = @documentid"
